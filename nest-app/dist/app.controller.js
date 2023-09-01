6d7bd1d453443659bf93edd60737e4774a435363
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-09-01 16:47:19 +1200

    bug fixes

--- nest-app/dist/app.controller.js
+++ nest-app/dist/app.controller.js
@@ -44,7 +44,7 @@ __decorate([
     __metadata("design:returntype", Array)
 ], AppController.prototype, "getArticles", null);
 __decorate([
-    (0, common_1.Get)('/api/articles:id'),
+    (0, common_1.Get)('/api/articles/:id'),
     __param(0, (0, common_1.Param)('id')),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [String]),
